fc5a41d67c95acdaf735fc239ca223b254a0c750
jmslbam <jmslbam@gmail.com>
2019-01-19 13:21:46 +0100

    switched back to Gulp version but with SCSS instead of LESS

---
+++ development/gulpfile.js
@@ -0,0 +1,221 @@
+// ## Globals
+var argv         = require('minimist')(process.argv.slice(2));
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync  = require('browser-sync').create();
+var changed      = require('gulp-changed');
+var concat       = require('gulp-concat');
+var flatten      = require('gulp-flatten');
+var gulp         = require('gulp');
+var gulpif       = require('gulp-if');
+var lazypipe     = require('lazypipe');
+var less         = require('gulp-less');
+var sass         = require('gulp-sass');
+var merge        = require('merge-stream');
+var cleanCSS     = require('gulp-clean-css');
+var plumber      = require('gulp-plumber');
+var runSequence  = require('run-sequence');
+var sourcemaps   = require('gulp-sourcemaps');
+var uglify       = require('gulp-uglify');
+var fs = require('fs');
+
+
+var manifest = require('../source/manifest.json');
+var browsersync = {};
+if (fs.existsSync('../source/browsersync.json')) {
+  browsersync = require('../source/browsersync.json');
+}
+
+// `path` - Paths to base asset directories. With trailing slashes.
+// - `path.source` - Path to the source files. Default: `assets/`
+// - `path.dist` - Path to the build directory. Default: `dist/`
+var path = manifest.paths;
+
+// `config` - Store arbitrary configuration values here.
+var config = manifest.config || {};
+config.browsersyncUrl =  browsersync.url || config.browsersyncUrl;
+
+// `globs` - These ultimately end up in their respective `gulp.src`.
+// - `globs.js` - Array of JS dependency objects. Example:
+//   ```
+//   {type: 'js', name: 'main.js', globs: []}
+//   ```
+// - `globs.css` - Array of CSS dependency objects. Example:
+//   ```
+//   {type: 'css', name: 'main.css', globs: []}
+//   ```
+// - `globs.fonts` - Array of font path globs.
+// - `globs.images` - Array of image path globs.
+
+var resources = manifest.resources;
+var globs = {};
+var project = {};
+resources.forEach(function(resource){
+  if(typeof globs[resource.type] == 'undefined'){
+    globs[resource.type] = [];
+  }
+  globs[resource.type].push(resource);
+  if(typeof project[resource.type] == 'undefined'){
+    project[resource.type] = [];
+  }
+  project[resource.type] = project[resource.type].concat(resource.globs);
+});
+globs.fonts = project.fonts;
+globs.images = project.images;
+
+// CLI options
+var enabled = {
+  // Disable source maps when `--production`
+  maps: !argv.production,
+  // Fail styles task on error when `--production`
+  failStyleTask: argv.production
+};
+
+// ## Reusable Pipelines
+// See https://github.com/OverZealous/lazypipe
+
+// ### CSS processing pipeline
+// Example
+// ```
+// gulp.src(cssFiles)
+//   .pipe(cssTasks('main.css')
+//   .pipe(gulp.dest(path.dist + 'styles'))
+// ```
+var cssTasks = function(filename) {
+  return lazypipe()
+    .pipe(function() {
+      return gulpif(!enabled.failStyleTask, plumber());
+    })
+    .pipe(function() {
+      return gulpif(enabled.maps, sourcemaps.init());
+    })
+    .pipe(function() {
+      return gulpif('*.scss', sass());
+      //return gulpif('*.less', less());
+    })
+    .pipe(concat, filename)
+    .pipe(autoprefixer, {
+      browsers: [
+        'last 2 versions',
+        'ie 9',
+        'android 2.3',
+        'android 4',
+        'opera 12'
+      ]
+    })
+    .pipe(cleanCSS, {
+      debug: true
+    })
+    .pipe(function() {
+      return gulpif(enabled.maps, sourcemaps.write('.'));
+    })();
+};
+
+
+// ### Write to rev manifest
+// If there are any revved files then write them to the rev manifest.
+// See https://github.com/sindresorhus/gulp-rev
+var writeToManifest = function(directory) {
+  return lazypipe()
+    .pipe(gulp.dest, path.dist + directory)
+    .pipe(browserSync.stream, {match: '**/*.{css}'})
+    .pipe(gulp.dest, path.dist)();
+};
+
+// ## Gulp tasks
+// Run `gulp -T` for a task summary
+
+// ### Styles
+// `gulp styles` - Compiles, combines, and optimizes CSS and project CSS.
+// By default this task will only log a warning if a precompiler error is
+// raised. If the `--production` flag is set: this task will fail outright.
+gulp.task('styles', function() {
+  var merged = merge();
+  globs.css.forEach(function(dep) {
+    var cssTasksInstance = cssTasks(dep.name);
+    if (!enabled.failStyleTask) {
+      cssTasksInstance.on('error', function(err) {
+        console.error(err.message);
+        this.emit('end');
+      });
+    }
+    merged.add(gulp.src(dep.globs, {base: 'styles'})
+      .pipe(cssTasksInstance));
+  });
+  return merged
+    .pipe(writeToManifest('styles'));
+});
+
+// ### Fonts
+// `gulp fonts` - Grabs all the fonts and outputs them in a flattened directory
+// structure. See: https://github.com/armed/gulp-flatten
+gulp.task('fonts', function() {
+  var sources = [];
+
+  for (i = 0; i < globs.fonts.length; i++) {
+    sources.push( globs.fonts[i] + '.eot');
+    sources.push( globs.fonts[i] + '.svg');
+    sources.push( globs.fonts[i] + '.ttf');
+    sources.push( globs.fonts[i] + '.woff');
+    sources.push( globs.fonts[i] + '.woff2');
+  }
+
+  return gulp.src(sources)
+    .pipe(flatten())
+    .pipe(gulp.dest(path.dist + 'fonts'))
+    .pipe(browserSync.stream());
+});
+
+// ### Images
+// `gulp images` - Run lossless compression on all the images.
+gulp.task('images', function() {
+  return gulp.src(globs.images)
+    .pipe(gulp.dest(path.dist + 'images'))
+    .pipe(browserSync.stream());
+});
+
+// ### Clean
+// `gulp clean` - Deletes the build folder entirely.
+gulp.task('clean', function(){
+  var del = require('del');
+
+  del([path.dist], {force:true}, function (err, paths) {
+  });
+});
+
+// ### Watch
+// `gulp watch` - Use BrowserSync to proxy your dev server and synchronize code
+// changes across devices. Specify the hostname of your dev server at
+// `manifest.config.devUrl`. When a modification is made to an asset, run the
+// build step for that asset and inject the changes into the page.
+// See: http://www.browsersync.io
+gulp.task('watch', function() {
+
+  browserSync.init({
+    files: ['../**/*.php', '../*.php', '../templates/**/*.twig', '../templates/*.twig'],
+    proxy: config.browsersyncUrl,
+    snippetOptions: {
+      whitelist: ['/wp-admin/admin-ajax.php'],
+      blacklist: ['/wp-admin/**']
+    }
+  });
+
+  gulp.watch([path.source + 'styles/**/*'], ['styles']);
+  gulp.watch([path.source + 'fonts/**/*'], ['fonts']);
+  gulp.watch([path.source + 'images/**/*'], ['images']);
+  gulp.watch([path.source + 'manifest.json'], ['build']);
+});
+
+// ### Build
+// `gulp build` - Run all the build tasks but don't clean up beforehand.
+// Generally you should be running `gulp` instead of `gulp build`.
+gulp.task('build', function(callback) {
+  runSequence('styles',
+              ['fonts', 'images'],
+              callback);
+});
+
+// ### Gulp
+// `gulp` - Run a complete build. To compile for production run `gulp --production`.
+gulp.task('default', ['clean'], function() {
+  gulp.start('build');
+});
